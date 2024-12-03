a7177144218129c222208dbdb3ce8c99d114ad25
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-12-02 22:00:21 -0300

    feat: create header and customer a project

---
+++ app/components/header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <div className="w-full bg-zinc-950 border-zinc-800 border-b z-10 fixed top-0 left-0">
+      <div className="w-full max-w-[1200px]  mx-auto px-4 pb-4  py-3 flex items-center justify-between">
+        <h1 className="font-semibold text-xl">Wendryus</h1>
+
+        <div className="flex gap-4 text-zinc-400">
+          <a className="header-link">Sobre</a>
+          <a className="header-link">Serviços</a>
+          <a className="header-link">Tecs</a>
+          <a className="header-link">Projetos</a>
+        </div>
+
+        <Link
+          target="_blank"
+          href="https://wa.me//5567999271900?text=Tenho%20interesse%20em%20desenvolver%20um%20projeto"
+        >
+          <Button variant="outline">Entre em contato</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
